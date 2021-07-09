714ea9358f8d587a03fcc5653e93ab9914c8fa25
Ejulu Wilson <emox1995@gmail.com>
2021-07-09 19:16:59 +0300

    added models to admin

--- Blogapp/api/admin.py
+++ Blogapp/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+# regidtering the article model
+@admin.register(Article) 
+
+#customising  the admin page
+# you can modyfy the admin panel using the overr 30 attributes of ModelAdmin 
+# below is an example
+
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description') #filter using thes columns
+    list_display = ('title', 'description')
--- Blogapp/api/models.py
+++ Blogapp/api/models.py
@@ -5,3 +5,11 @@ class Article(models.Model): #Article is the table
     #the following attribites are the fields
     title = models.CharField(max_length= 100)
     description = models.TextField()
+
+    '''
+    By default, django assingd an id to the object and returns 
+    it as the link name in the admin panel
+    '''
+    #add the following to give it your own name
+    def __str__(self):
+        return f'{self.title}'

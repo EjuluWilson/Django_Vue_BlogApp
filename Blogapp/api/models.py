86928c42d3c3f53699e0638b32b64726dcdb21e6
Ejulu Wilson <emox1995@gmail.com>
2021-07-09 09:52:35 +0300

    created the article model

--- Blogapp/api/models.py
+++ Blogapp/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): #Article is the table
+    #the following attribites are the fields
+    title = models.CharField(max_length= 100)
+    description = models.TextField()

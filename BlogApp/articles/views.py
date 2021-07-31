d791d9122efd5195a273e720879832e2b5de1366
Ejulu Wilson <emox1995@gmail.com>
2021-07-31 13:08:45 +0300

    createda  vieset for the article model

--- BlogApp/articles/views.py
+++ BlogApp/articles/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Article
-from .serializers import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing the article model.
+    """
+    queryset = Article.objects.all().order_by("-created_at")
+    lookup_field = "slug"
+    serializer_class = ArticleSerializer

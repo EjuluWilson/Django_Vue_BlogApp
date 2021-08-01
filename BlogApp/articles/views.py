dbbfda5a320df5c493ff3fe2fccf318ded1bc6bf
Ejulu Wilson <emox1995@gmail.com>
2021-08-01 08:46:54 +0300

    added permisions to viewset

--- BlogApp/articles/views.py
+++ BlogApp/articles/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthor
 # Create your views here.
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,9 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all().order_by("-created_at")
     lookup_field = "slug"
     serializer_class = ArticleSerializer
+
+    #adding the custom permission
+    permission_classes = [IsAuthenticated, IsAuthor]
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
